ad94c8485fec49e2c4d37e2e4781fb36c870a04a
mslijepc <mladen.slijepcevic@sifive.com>
2025-04-29 14:49:52 +0200

    upd main

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,9 @@
 #include "s2_demo_proc.h"
 #include "memory_simulator.h"
 #include "riscv/cfg.h"
+#include <filesystem>
+
+#define START_PC 0x20000000
 
 int main() {
     // configuration time
@@ -10,19 +13,17 @@ int main() {
     cfg.priv = "MSU";        // Machine, Supervisor, and User privilege levels
     cfg.misaligned = false;  // Don't allow misaligned memory accesses
     cfg.endianness = endianness_little;  // Little endian
-    cfg.start_pc = 0x20000000;  // Start PC
-    
-    // Configure memory layout
+    cfg.start_pc = START_PC;  // Start PC
     cfg.mem_layout.clear();
     cfg.pmpregions = 16;
     cfg.hartids = {0};
 
     // creating external simulator
     #ifdef USE_BRIDGE
-    memory_simulator mem_sim(1024 * 1024 * 1024);
+    memory_simulator mem_sim(1024 * 1024 * 1024, START_PC);
     spike_bridge_t ext_sim(&mem_sim);
     #else
-    memory_simulator_wrapper ext_sim(1024 * 1024 * 1024); // TODO: update naming
+    memory_simulator_wrapper ext_sim(1024 * 1024 * 1024, START_PC); // TODO: update naming
     #endif
     // setting cfg field from external simulator
     cfg.external_simulator = &ext_sim;
@@ -33,12 +34,16 @@ int main() {
     // this is runtime from this point
     // first reset and load elf
     spike_proc.reset();
-    ext_sim.load_elf_file("vec.elf");
-    // ext_sim.load_elf_file("dhrystone.elf");
+    // load elf
+    if (!std::filesystem::exists("sw/main.elf")) {
+        std::cerr << "Error: sw/main.elf not found. Please build the software first.\n";
+        exit(1);
+    }
+    ext_sim.load_elf_file("sw/main.elf");
     // enable debugging features
     spike_proc.enable_debug();
     spike_proc.configure_log(true, false);
-    // execute 50 instructions
+
     while (1)
         spike_proc.step(50);
 
