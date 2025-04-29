57a29b8a5533a45746b41135f2418119a9c5935c
mslijepc <mladen.slijepcevic@sifive.com>
2025-04-29 14:49:34 +0200

    upd memory_simulator

--- src/memory_simulator.cc
+++ src/memory_simulator.cc
@@ -2,7 +2,7 @@
 #include <cstring>
 #include <cassert>
 
-memory_simulator::memory_simulator(uint64_t size): mem_size(size) {
+memory_simulator::memory_simulator(uint64_t size, uint64_t start_pc): mem_size(size), start_pc(start_pc) {
     printf("creating memory_simulator\n");
     set_rom_contents();
 }
@@ -10,6 +10,19 @@ memory_simulator::memory_simulator(uint64_t size): mem_size(size) {
 memory_simulator::~memory_simulator() {}
 
 void memory_simulator::write(uint64_t addr, const uint8_t* data, size_t len) {
+
+    if (unlikely(addr == 0x3fffb008)) {
+      const unsigned int testStatus = ((unsigned int*)data)[0];
+
+      if (testStatus != 0) {
+        if (testStatus == 0x5555) {
+          printf("PASS\n");
+        } else {
+          printf("FAIL with status %x\n", testStatus);
+        }
+        assert(0);
+      } 
+    } 
     sparse_arr.write(addr, data, len);
 }
 
@@ -34,8 +47,10 @@ uint64_t memory_simulator::size() const {
     return mem_size;
 }
 
+// Sets up initial ROM contents with reset vector and boot code
 void memory_simulator::set_rom_contents() {
-    uint64_t reset_vect_0 = 0x20000000;
+    uint64_t reset_vect_0 = start_pc;
+
     uint8_t reset_vec_size = 8;
     std::vector<uint32_t> reset_vec_data;
     reset_vec_data.reserve(reset_vec_size);
@@ -59,8 +74,8 @@ void memory_simulator::set_rom_contents() {
 }
 
 
-//////////////////
-memory_simulator_wrapper::memory_simulator_wrapper(uint64_t size): memory_simulator(size) {
+////////////////// wrapper
+memory_simulator_wrapper::memory_simulator_wrapper(uint64_t size, uint64_t start_pc): memory_simulator(size, start_pc) {
     printf("creating memory_simulator_wrapper\n");
 }
 
@@ -68,24 +83,28 @@ memory_simulator_wrapper::~memory_simulator_wrapper() {
 }
 
 bool memory_simulator_wrapper::load(reg_t addr, size_t len, uint8_t* bytes) {
-    if (addr + len > mem_size) {
-        return false;
-    }
-    sparse_arr.read(addr, bytes, len);
+    // if (addr + len > mem_size) {
+    //     return false;
+    // }
+    memory_simulator::read(addr, bytes, len);
+    // sparse_arr.read(addr, bytes, len);
     return true;
 }
 
 bool memory_simulator_wrapper::store(reg_t addr, size_t len, const uint8_t* bytes) {
-    if (addr + len > mem_size) {
-        return false;
-    }
-    sparse_arr.write(addr, bytes, len);
+    // if (addr + len > mem_size) {
+    //     printf("store addr = %lx\n", addr);
+    //     printf("store len = %lx\n", len);
+    //     return false;
+    // }
+    memory_simulator::write(addr, bytes, len);
     return true;
 }
 
-//////////////
+////////////// bridge
 
-spike_bridge_t::spike_bridge_t(memory_simulator* sim) : sim(sim) {
+spike_bridge_t::spike_bridge_t(memory_simulator* sim, uint64_t start_pc) : sim(sim) {
+    sim->set_start_pc(start_pc);
     printf("creating spike_bridge_t\n");
 }
 
--- src/memory_simulator.h
+++ src/memory_simulator.h
@@ -9,29 +9,33 @@
 #include <cstdint>
 #include "riscv/devices.h"
 
-// base class, has all functionalities of a memory simulator
+// base class, has read/write functionalities we need for a simple memory simulator
 class memory_simulator {
 public:
-    memory_simulator(uint64_t size);
+    memory_simulator(uint64_t size, uint64_t start_pc = 0x20000000);
     virtual ~memory_simulator();
 
     void write(uint64_t addr, const uint8_t* data, size_t len);
     void read(uint64_t addr, uint8_t* data, size_t len);
     std::map<std::string, uint64_t> load_elf_file(const std::string& filename, uint64_t* entry_point = nullptr);
-    void load_hex_file(const std::string& filename);
+    void load_hex_file(const std::string& filename); // not implemented yet
     uint64_t size() const;
     void set_rom_contents();
+    void set_start_pc(uint64_t start_pc) { this->start_pc = start_pc; }
 
 protected:
     uint64_t mem_size;
+    uint64_t start_pc;
+private:
     util::sparse_array<uint8_t, 18, 30> sparse_arr;
 };
 
 /// one way is to create a wrapper class
 class memory_simulator_wrapper : public memory_simulator, public abstract_sim_if_t {
     public:
-        memory_simulator_wrapper(uint64_t size);
+        memory_simulator_wrapper(uint64_t size, uint64_t start_pc = 0x20000000);
         ~memory_simulator_wrapper() override;
+        // from abstract_sim_if_t
         bool load(reg_t addr, size_t len, uint8_t* bytes) override;
         bool store(reg_t addr, size_t len, const uint8_t* bytes) override;
 };
@@ -40,15 +44,14 @@ class memory_simulator_wrapper : public memory_simulator, public abstract_sim_if
 class spike_bridge_t : public abstract_sim_if_t {
 public:
     // class name of external simulator is known by the end used
-    spike_bridge_t(memory_simulator* sim);
+    spike_bridge_t(memory_simulator* sim, uint64_t start_pc = 0x20000000);
+    virtual ~spike_bridge_t() = default;
+    spike_bridge_t() = delete;
+    spike_bridge_t(const spike_bridge_t&) = delete;
 
     // from abstract_sim_if_t
     bool load(reg_t addr, size_t len, uint8_t* bytes) override;
     bool store(reg_t addr, size_t len, const uint8_t* bytes) override;
-
-    virtual ~spike_bridge_t() = default;
-    spike_bridge_t() = delete;
-    spike_bridge_t(const spike_bridge_t&) = delete;
 
     void load_elf_file(const std::string& filename);
 
