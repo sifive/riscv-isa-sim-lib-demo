0eb1152ad66a7c06cbd40262be9bb0b7948854e7
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2025-10-24 19:23:55 -0700

    fixed license to apache

--- src/systemc/util/dbg_component.h
+++ src/systemc/util/dbg_component.h
@@ -1,21 +1,16 @@
-
-// Copyright (c) 2023 SiFive, Inc. -- Proprietary and Confidential
-// All Rights Reserved.
+// Copyright 2025 SiFive
 //
-// NOTICE: All information contained herein is, and remains the
-// property of SiFive, Inc. The intellectual and technical concepts
-// contained herein are proprietary to SiFive, Inc. and may be covered
-// by U.S. and Foreign Patents, patents in process, and are protected by
-// trade secret or copyright law.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// This work may not be copied, modified, re-published, uploaded,
-// executed, or distributed in any way, in any medium, whether in whole
-// or in part, without prior written permission from SiFive, Inc.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// The copyright notice above does not evidence any actual or intended
-// publication or disclosure of this source code, which includes
-// information that is confidential and/or proprietary, and is a trade
-// secret, of SiFive, Inc.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef DBG_C
 #define DBG_C
